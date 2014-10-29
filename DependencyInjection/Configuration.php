016c6b814f888ca356e45817edea2375323172a8
Kruske <info@kruske.es>
2014-10-29 18:16:41 +0100

    asdasd

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,38 +1,1 @@
-<?php
-
-namespace Kruske\KruskeTPVVirtualBBVABundle\DependencyInjection;
-
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
-
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
-class Configuration implements ConfigurationInterface
-{
-    /**
-     * {@inheritDoc}
-     */
-    public function getConfigTreeBuilder()
-    {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('kruske_tpv_virtual_bbva');
-
-        $rootNode
-            ->children()
-                ->scalarNode('url_tpv')->isRequired()->end()
-                ->scalarNode('clave_tpv')->isRequired()->end()
-                ->scalarNode('name_tpv')->isRequired()->end()
-                ->scalarNode('code_tpv')->isRequired()->end()
-                ->scalarNode('terminal_tpv')->isRequired()->end()
-                ->scalarNode('currency_tpv')->isRequired()->end()
-                ->scalarNode('transaction_type_tpv')->isRequired()->end()
-                ->scalarNode('url_merchant_tpv')->isRequired()->end()
-            ->end()
-        ;
-
-        return $treeBuilder;
-    }
-}
+<?phpnamespace Kruske\KruskeTPVVirtualBBVABundle\DependencyInjection;use Symfony\Component\Config\Definition\Builder\TreeBuilder;use Symfony\Component\Config\Definition\ConfigurationInterface;/** * This is the class that validates and merges configuration from your app/config files * * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class} */class Configuration implements ConfigurationInterface {    /**     * {@inheritDoc}     */    public function getConfigTreeBuilder() {        $treeBuilder = new TreeBuilder();        $rootNode = $treeBuilder->root('kruske_tpv_virtual_bbva');        $rootNode                ->children()                ->scalarNode('url_tpv')->isRequired()->end()                ->scalarNode('clave_tpv')->isRequired()->end()                ->scalarNode('name_tpv')->isRequired()->end()                ->scalarNode('code_tpv')->isRequired()->end()                ->scalarNode('terminal_tpv')->isRequired()->end()                ->scalarNode('currency_tpv')->isRequired()->end()                ->scalarNode('transaction_type_tpv')->isRequired()->end()                ->scalarNode('url_merchant_tpv')->isRequired()->end()                ->end()        ;        return $treeBuilder;    }}
